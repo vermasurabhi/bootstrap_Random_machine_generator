1dccbb6ff19365bdc93147c0c998680f5833b999
vermasurabhi <127035688+vermasurabhi@users.noreply.github.com>
2023-03-05 21:19:14 +0530

    Add files via upload

---
+++ quotestore.js
@@ -0,0 +1,154 @@
+const QUOTESTORE = [
+    {
+       quote: "Those freckles make you seem like a galaxy of stars, just waiting to be explored and loved.",
+       author: "Nikita Gill"
+    },
+    {  
+       quote:"The only people who never fail are those who never try.",
+       author:"Ilka Chase"
+    },
+    {  
+       quote:"Failure is just another way to learn how to do something right.",
+       author:"Marian Wright Edelman"
+    },
+    {  
+       quote:"I failed my way to success.",
+       author:"Thomas Edison"
+    },
+    {  
+       quote:"Every failure brings with it the seed of an equivalent success.",
+       author:"Napoleon Hill"
+    },
+    {  
+       quote:"Only those who dare to fail greatly can ever achieve greatly.",
+       author:"John F. Kennedy"
+    },
+    {  
+       quote:"It is hard to fail, but it is worse never to have tried to succeed.",
+       author:"Theodore Roosevelt"
+    },
+    {  
+       quote:"Imagination is more important than knowledge.",
+       author:"Albert Einstein"
+    },
+    {  
+       quote:"Hold fast to dreams, for if dreams die, life is a broken winged bird that cannot fly.",
+       author:"Langston Hughes"
+    },
+    {  
+       quote:"The future belongs to those who believe in the beauty of their dreams.",
+       author:"Eleanor Roosevelt"
+    },
+    {  
+       quote:"Go confidently in the direction of your dreams. Live the life you have imagined.",
+       author:"Henry David Thoreau"
+    },
+    {  
+       quote:"You cannot depend on your eyes when your imagination is out of focus.",
+       author:"Mark Twain"
+    },
+    {  
+       quote:"Commitment leads to action. Action brings your dream closer.",
+       author:"Marcia Wieder"
+    },
+    {  
+       quote:"I never think of the future",
+       author:"it comes soon enough.\""
+    },
+    {  
+       quote:"Don't waste your life in doubts and fears: Spend yourself on the work before you, well assured that the right performance of this hour's duties will be the best preparation for the hours or ages that follow it.",
+       author:"Ralph Waldo Emerson"
+    },
+    {  
+       quote:"Do not anticipate trouble or worry about what may never happen. Keep in the sunlight.",
+       author:"Benjamin Franklin"
+    },
+    {  
+       quote:"Take time to deliberate; but when the time for action arrives, stop thinking and go in.",
+       author:"Andrew Jackson"
+    },
+    {  
+       quote:"The mind that is anxious about future events is miserable.",
+       author:"Seneca"
+    },
+    {  
+       quote:"Live in each season as it passes; breathe the air, drink the drink, taste the fruit, and resign yourself to the influences of each.",
+       author:"Henry David Thoreau"
+    },
+    {  
+       quote:"The art of leadership is saying no, not yes. It is very easy to say yes.",
+       author:"Tony Blair"
+    },
+    {  
+       quote:"A leader is a dealer in hope.",
+       author:"Napoleon Bonaparte"
+    },
+    {  
+       quote:"While a good leader sustains momentum, a great leader increases it.",
+       author:"John C. Maxwell"
+    },
+    {  
+       quote:"A general is just as good or just as bad as the troops under his command make him.",
+       author:"General Douglas MacArthur"
+    },
+    {  
+       quote:"To do great things is difficult; but to command great things is more difficult.",
+       author:"Friedrich Nietzsche"
+    },
+    {  
+       quote:"Leadership does not always wear the harness of compromise.",
+       author:"Woodrow Wilson"
+    },
+    {  
+       quote:"Business opportunities are like buses",
+       author:"there's always another one coming.\""
+    },
+    {  
+       quote:"I avoid looking forward or backward, and try to keep looking upward.",
+       author:"Charlotte Bronte"
+    },
+    {  
+       quote:"The more difficulties one has to encounter, within and without, the more significant and the higher in inspiration his life will be.",
+       author:"Horace Bushnell"
+    },
+    {  
+       quote:"Every artist was first an amateur.",
+       author:"Ralph Waldo Emerson"
+    },
+    {  
+       quote:"I was always looking outside myself for strength and confidence, but it comes from within. It is there all of the time.",
+       author:"Anna Freud"
+    },
+    {  
+       quote:"We can do anything we want to do if we stick to it long enough.",
+       author:"Helen Keller"
+    },
+    {  
+       quote:"Our business in life is not to get ahead of others, but to get ahead of ourselves.",
+       author:"E. Joseph Cossman"
+    },
+    {  
+       quote:"Insist on yourself. Never imitate.",
+       author:"Ralph Waldo Emerson"
+    },
+    {  
+       quote:"Who looks outside, dreams. Who looks inside, awakes.",
+       author:"Carl Jung"
+    },
+    {  
+       quote:"It is not easy to find happiness in ourselves, and it is not possible to find it elsewhere.",
+       author:"Agnes Repplier"
+    },
+    {  
+       quote:"The only journey is the one within.",
+       author:"Rainer Maria Rilke"
+    },
+    {  
+       quote:"Follow your honest convictions, and stay strong.",
+       author:"William Thackeray"
+    }  
+  ]
+  
+ 
+ 
+ 
